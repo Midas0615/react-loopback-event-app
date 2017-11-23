d948888fea21e17ce432af89643c9cdcf1acf075
dreamweaver220 <dreamweaver995@gmail.com>
2017-11-23 11:23:19 +0100

    random

--- client/src/routes/Docs/index.js
+++ client/src/routes/Docs/index.js
@@ -1,3 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+import Color from 'color'
+
+
+
+const darken = (prop, amt) => props => {
+  return Color(props.theme[prop]).darken(amt).string();
+}
+
+const lighten = (prop, amt) => props => {
+  return Color(props.theme[prop]).lighten(amt).string();
+}
+
+
+
+const ColoredButton = styled.button`
+ background: ${ darken('primary', 1.3) };
+`
+
+
 export default () =>
-<div>docs</div>
+<div>
+  <ColoredButton>Button</ColoredButton>
+</div>
